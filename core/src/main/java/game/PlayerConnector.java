b3d5dd2e8eabc74f4ca25cfeb9ed352b3ab1d2f0
typsoon <p.sitek228@gmail.com>
2025-05-23 13:43:34 +0200

    Patch regarding codegen

--- core/src/main/java/game/PlayerConnector.java
+++ core/src/main/java/game/PlayerConnector.java
@@ -6,6 +6,8 @@ import java.util.Collection;
 
 public interface PlayerConnector {
     void subscribe(ActionReceiver receiver);
+
     void unsubscribe(ActionReceiver receiver);
+
     void sendGameState(Collection<GameState> gameStates);
 }
