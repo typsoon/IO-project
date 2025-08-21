552b4deccf44fbbec860f64cd1dbcdc262838338
mprzebieracz <m.przebieracz@student.uj.edu.pl>
2025-08-21 12:02:22 +0200

    Fix

--- core/src/main/java/network/messages/userstate/GameConfirmation.java
+++ core/src/main/java/network/messages/userstate/GameConfirmation.java
@@ -2,7 +2,10 @@ package network.messages.userstate;
 
 import game.utility.ISendable;
 
-public enum GameConfirmation implements ISendable {
-    CONFIRM,
-    CANCEL
+public record GameConfirmation(Confirmation confirmation) implements ISendable {
+    public enum Confirmation {
+        CONFIRMED,
+        CANCELLED,
+    }
 }
+
